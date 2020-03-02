315d659eb19d1689b3f349a6a79b1574537ce87a
Sudhanshu <perfectsudh@gmail.com>
2020-03-02 15:45:49 +0530

    actions, reducers, selectors and store dir structure created

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { ensureDir, ensureFile } from "fs-extra"
+import path from "path"
+
+const createStructure = () => {
+  if (process.argv.length < 3) {
+    console.log("expected argument (path to your src folder)")
+    return
+  }
+
+  const start = process.argv[2]
+
+  // build actions structure
+  ensureDir(path.join(start, "actions"))
+    .then(() => {
+      console.log("actions dir created")
+    })
+    .then(() => {
+      // create index file
+      ensureFile(path.join(start, "actions", "index.js")).then(() => {
+        console.log("actions/index.js created")
+      })
+    })
+
+  ensureDir(path.join(start, "reducers"))
+    .then(() => {
+      console.log("reducers dir created")
+    })
+    .then(() => {
+      // create index file
+      ensureFile(path.join(start, "reducers", "index.js")).then(() => {
+        console.log("reducers/index.js created")
+      })
+    })
+
+  ensureDir(path.join(start, "selectors"))
+    .then(() => {
+      console.log("selectors dir created")
+    })
+    .then(() => {
+      // create index file
+      ensureFile(path.join(start, "selectors", "index.js")).then(() => {
+        console.log("selectors/index.js created")
+      })
+    })
+
+  ensureFile(path.join(start, "store.js")).then(() => {
+    console.log("store.js created")
+  })
+}
+
+createStructure()
